9bb1a926d69721818781de9fe50bc358108c02ad
DanieldeR <derepentigny.daniel@gmail.com>
2021-07-01 15:06:09 -0400

    All change

--- analysis.py
+++ analysis.py
@@ -1,23 +1,27 @@
 #%%
 import pandas as pd
 from random import sample, choice
+import numpy as np 
 
 # %%
-word_list = pd.read_csv('resources/english3.txt', sep='\n', header=None)
-frequency = pd.read_csv('resources/unigram_freq.csv')
 
-word_list.columns = ['words']
+def clean():
+    word_list = pd.read_csv('resources/english3.txt', sep='\n', header=None)
+    frequency = pd.read_csv('resources/unigram_freq.csv')
 
-# %%
-df = pd.merge(word_list, frequency, how='left', left_on='words', right_on='word')
-df = df.dropna()
+    word_list.columns = ['words']
 
-df = df.sort_values(by='count', ascending=0)
+    df = pd.merge(word_list, frequency, how='left', left_on='words', right_on='word')
+    df = df.dropna()
 
-cleaned = df['word']
+    df = df.sort_values(by='count', ascending=0)
 
-cleaned = cleaned[cleaned.map(lambda x: len(list(x)) >= 4)]
-cleaned = cleaned.reset_index(drop=True)
+    cleaned = df['word']
+
+    cleaned = cleaned[cleaned.map(lambda x: len(list(x)) >= 4)]
+    cleaned = cleaned.reset_index(drop=True)
+
+    return cleaned
 
 
 # %%
@@ -29,7 +33,7 @@ def get_letters(v:int = 3):
     return (list(sample(consonants, k=c)) + list(sample(vowels, k=v)))
 # %%
 
-def get_word_list():
+def get_word_list(cleaned):
     found = False
     
     while not found:
@@ -54,4 +58,18 @@ def get_word_list():
             found = True
 
     return((letter, letters, words_to_spell))
+
+#%%
+def stats(letter, letters, words):
+    difficulty = np.mean(list(words.index))
+
+    return (difficulty, len(words))
+# %%
+if __name__ == '__main__':
+    data = clean()
+
+    letter, letters, words = get_word_list(data)
+
+    print(letter, letters)
+    print(stats(letter, letters, words))
 # %%
